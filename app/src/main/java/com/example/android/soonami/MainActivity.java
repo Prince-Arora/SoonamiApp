fc2793652a0f19a7cbf23d414f2a11033d256b3e
Prince-Arora <19princearora95@gmail.com>
2017-12-28 19:27:52 +0530

    add Null

--- app/src/main/java/com/example/android/soonami/MainActivity.java
+++ app/src/main/java/com/example/android/soonami/MainActivity.java
@@ -18,6 +18,7 @@ package com.example.android.soonami;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 
@@ -154,6 +155,8 @@ public class MainActivity extends AppCompatActivity {
          */
         private String makeHttpRequest(URL url) throws IOException {
             String jsonResponse = "";
+            if(url==null)
+                return jsonResponse;
             HttpURLConnection urlConnection = null;
             InputStream inputStream = null;
             try {
@@ -162,9 +165,10 @@ public class MainActivity extends AppCompatActivity {
                 urlConnection.setReadTimeout(10000 /* milliseconds */);
                 urlConnection.setConnectTimeout(15000 /* milliseconds */);
                 urlConnection.connect();
+                if(urlConnection.getResponseCode()==200){
                 inputStream = urlConnection.getInputStream();
                 jsonResponse = readFromStream(inputStream);
-            } catch (IOException e) {
+            }} catch (IOException e) {
                 // TODO: Handle the exception
             } finally {
                 if (urlConnection != null) {
@@ -201,6 +205,8 @@ public class MainActivity extends AppCompatActivity {
          * about the first earthquake from the input earthquakeJSON string.
          */
         private Event extractFeatureFromJson(String earthquakeJSON) {
+            if(TextUtils.isEmpty(earthquakeJSON))
+                return null;
             try {
                 JSONObject baseJsonResponse = new JSONObject(earthquakeJSON);
                 JSONArray featureArray = baseJsonResponse.getJSONArray("features");
